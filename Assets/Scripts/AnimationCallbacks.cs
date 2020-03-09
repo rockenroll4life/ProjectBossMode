a13f9bdf664028b82da2529fc6384643a452a0d3
rockenroll4life <rockenroll4life@gmail.com>
2020-03-09 03:14:08 -0400

    New Model with animations, support for super basic 'roll' and an updated animation controller

---
+++ Assets/Scripts/AnimationCallbacks.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCallbacks : MonoBehaviour {
+    Player player;
+    void Start() {
+        player = GetComponentInParent<Player>();
+    }
+
+    public void StartRoll() {
+        player.StartRoll();
+    }
+
+    public void FinishRoll() {
+        player.FinishRoll();
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,28 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    enum MovementState {
+        Moving,   //  For now this encapsulates Idle / Walking / Running (Walking is just moving with a speed of 0!)
+        Rolling,
+    }
+
     public float movementSpeed = 5f;
-    public AnimationCurve moveSpeedCurve;
 
     Camera cam;
     Animator animator;
-    Rigidbody rigidbody;
-
+    Rigidbody physicsbody;
     InputManager input;
+
+    MovementState movementState;
 
     Vector3 camOffset;
 
     //  TODO: Remove this?
-    Vector3 currentMovingVelocity;
-    bool pressed = false;
+    Vector3 currentMovingVelocity = Vector3.forward;
 
-    void Start(){
+    void Start() {
         cam = Camera.main;
 
         camOffset = cam.transform.position - transform.position;
 
         animator = GetComponentInChildren<Animator>();
-        rigidbody = GetComponent<Rigidbody>();
+        physicsbody = GetComponent<Rigidbody>();
 
         input = new InputManager(0);
     }
@@ -33,7 +37,8 @@ public class Player : MonoBehaviour {
         //  Update our input manager to track inputs
         input.update();
 
-        CheckInput();
+        UpdateState();
+
         UpdateAnimations();
     }
 
@@ -41,28 +46,51 @@ public class Player : MonoBehaviour {
         cam.transform.position = transform.position + camOffset;
     }
 
-    void CheckInput() {
-        //  TODO: We need to add movement states to support things such as rolling
+    void UpdateState() {
+        if (movementState == MovementState.Moving) {
+            //  Check for movement Input
 
-        //  Movement
-        currentMovingVelocity = input.getStick(InputManager.Stick.Left);
-        if (currentMovingVelocity.sqrMagnitude > 0.1 * 0.1) {
-            float val = Mathf.Max(Mathf.Abs(currentMovingVelocity.x), Mathf.Abs(currentMovingVelocity.z));
-            float actualSpeed = moveSpeedCurve.Evaluate(val);
+            //  Movement / Rotation
+            currentMovingVelocity = input.getStick(InputManager.Stick.Left);
+            if (currentMovingVelocity.sqrMagnitude > 0.1 * 0.1) {
+                //  Move the player
+                physicsbody.MovePosition(transform.position + (currentMovingVelocity.normalized * movementSpeed * Time.deltaTime));
 
-            //  Move the player
-            rigidbody.MovePosition(transform.position + (currentMovingVelocity.normalized * (actualSpeed * movementSpeed) * Time.deltaTime));
-            
-            //  Rotate the player
-            transform.rotation = Quaternion.LookRotation(currentMovingVelocity);
+                //  Rotate the player
+                transform.rotation = Quaternion.LookRotation(currentMovingVelocity);
+            }
+
+            //  Rolling
+            if (input.isDown(InputManager.Buttons.A)) {
+                movementState = MovementState.Rolling;
+            }
         }
 
-        pressed = input.isDown(InputManager.Buttons.A);
+        if (movementState ==  MovementState.Rolling) {
+            //  TODO: Turn this move into a burst (explosive) move, it's a dive roll...act awesome!
+
+            //  Since we're rolling we can't change directions, so just roll in the last direction we were looking
+            physicsbody.MovePosition(transform.position + (transform.forward * movementSpeed * Time.deltaTime));
+        }
     }
 
     void UpdateAnimations() {
         //  Update the players movement animation
-        float speedPercent = Mathf.Max(Mathf.Abs(currentMovingVelocity.x), Mathf.Abs(currentMovingVelocity.z)) / 1f;
-        animator.SetFloat("speedPercent", speedPercent, 0.1f, Time.deltaTime);
+        float speedPercent = (Mathf.Abs(currentMovingVelocity.x) + Mathf.Abs(currentMovingVelocity.z)) > Mathf.Epsilon ? 1 : 0;
+        animator.SetFloat("speedPercent", speedPercent , 0.1f, Time.deltaTime);
+
+        //   Update animation controller rolling state
+        animator.SetBool("isRolling", movementState == MovementState.Rolling);
+    }
+
+    public void StartRoll() {
+        Debug.Log("Roll Started");
+    }
+
+    public void FinishRoll() {
+        Debug.Log("Roll Finished");
+
+        //  We finished our roll animation so transition back to our move state
+        movementState = MovementState.Moving;
     }
 }
