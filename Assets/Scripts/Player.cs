71fc936422f623f2dc19cab771b3f93207879335
rockenroll4life <rockenroll4life@gmail.com>
2020-03-09 04:15:53 -0400

    Improved the in game rolling, now feels more rolly!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,10 @@ public class Player : MonoBehaviour {
         Rolling,
     }
 
-    public float movementSpeed = 5f;
+    //  TODO: [Rock]: Cleanup the order of our variables...too lazy and will mess it up while I'm starting to dev this
+    readonly float movementSpeed = 25f;
+    readonly float rollSpeed = 40f;
+    readonly float rotationSpeed = 720f;
 
     Camera cam;
     Animator animator;
@@ -19,8 +22,8 @@ public class Player : MonoBehaviour {
 
     Vector3 camOffset;
 
-    //  TODO: Remove this?
-    Vector3 currentMovingVelocity = Vector3.forward;
+    Vector3 movingDir = Vector3.forward;
+    bool moving = false;
 
     void Start() {
         cam = Camera.main;
@@ -37,6 +40,8 @@ public class Player : MonoBehaviour {
         //  Update our input manager to track inputs
         input.update();
 
+        CheckInput();
+
         UpdateState();
 
         UpdateAnimations();
@@ -46,50 +51,59 @@ public class Player : MonoBehaviour {
         cam.transform.position = transform.position + camOffset;
     }
 
+    void CheckInput() {
+        if (movementState == MovementState.Moving) {
+            //  Rolling
+            if (input.isDown(InputManager.Buttons.A)) {
+                movementState = MovementState.Rolling;
+
+                //  Because we're going to do a roll, just snap the players rotation instead of doing the lerp like normal
+                transform.rotation = Quaternion.LookRotation(movingDir);
+            }
+        }
+    }
+
     void UpdateState() {
         if (movementState == MovementState.Moving) {
             //  Check for movement Input
 
             //  Movement / Rotation
-            currentMovingVelocity = input.getStick(InputManager.Stick.Left);
-            if (currentMovingVelocity.sqrMagnitude > 0.1 * 0.1) {
+            Vector3 moveVelocity = input.getStick(InputManager.Stick.Left);
+            if (moveVelocity.sqrMagnitude > 0.1 * 0.1) {
+                movingDir = moveVelocity.normalized;
+                moving = true;
+
                 //  Move the player
-                physicsbody.MovePosition(transform.position + (currentMovingVelocity.normalized * movementSpeed * Time.deltaTime));
-
-                //  Rotate the player
-                transform.rotation = Quaternion.LookRotation(currentMovingVelocity);
+                physicsbody.MovePosition(transform.position + (movingDir * movementSpeed * Time.deltaTime));
+            } else {
+                moving = false;
             }
 
-            //  Rolling
-            if (input.isDown(InputManager.Buttons.A)) {
-                movementState = MovementState.Rolling;
-            }
+            //  Rotate the player
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(movingDir), rotationSpeed * Time.deltaTime);
         }
 
-        if (movementState ==  MovementState.Rolling) {
-            //  TODO: Turn this move into a burst (explosive) move, it's a dive roll...act awesome!
-
+        if (movementState == MovementState.Rolling) {
             //  Since we're rolling we can't change directions, so just roll in the last direction we were looking
-            physicsbody.MovePosition(transform.position + (transform.forward * movementSpeed * Time.deltaTime));
+            physicsbody.MovePosition(transform.position + (transform.forward * rollSpeed * Time.deltaTime));
+
+            //  TOOD: [Rock]: Decide if we want to allow for turning while we roll? Leaning towards no, but worth a thought
         }
     }
 
     void UpdateAnimations() {
         //  Update the players movement animation
-        float speedPercent = (Mathf.Abs(currentMovingVelocity.x) + Mathf.Abs(currentMovingVelocity.z)) > Mathf.Epsilon ? 1 : 0;
-        animator.SetFloat("speedPercent", speedPercent , 0.1f, Time.deltaTime);
+        animator.SetFloat("speedPercent", moving ? 1 : 0 , 0.1f, Time.deltaTime);
 
         //   Update animation controller rolling state
         animator.SetBool("isRolling", movementState == MovementState.Rolling);
     }
 
     public void StartRoll() {
-        Debug.Log("Roll Started");
+        //  TODO: [Rock]: Do we actually need this?
     }
 
     public void FinishRoll() {
-        Debug.Log("Roll Finished");
-
         //  We finished our roll animation so transition back to our move state
         movementState = MovementState.Moving;
     }
