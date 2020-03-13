d7fb4e672b5af9ed4ceac58b21a3636f1bd4f398
rockenroll4life <rockenroll4life@gmail.com>
2020-03-13 04:13:47 -0400

    A few GameEvents for Using and updating the cooldown of abilities

---
+++ Assets/Scripts/Abilities/AbilityManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityManager : MonoBehaviour {
+    static readonly int NUM_ABILITIES = 5;
+
+    //
+    public Ability[] abilities = new Ability[NUM_ABILITIES];
+}
--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -1,12 +1,19 @@
 ﻿public enum GameEvents {
     KeyboardButton_Pressed = 0,
     //  We're reserving the first 512 ids for KeyCode support, probably don't really need this many but that's what we're doing...
+    //  Just for safety we'll resume the values at 600
 
     //  Mouse Clicks
     Mouse_LeftClick = 600,
     Mouse_RightClick = 601,
     Mouse_MiddleClick = 602,
 
-    Health_Changed = 603,
-    Mana_Changed = 604,
+    //  Ability Use - 10 values reserved (QWERT + Ctrl-QWERT)
+    Ability_Use = 603,
+
+    //  Updating Ability Cooldowns - 10 values reserved (QWERT + Ctrl-QWERT)
+    Ability_Cooldown_Update = 613,
+
+    Health_Changed = 623,
+    Mana_Changed = 624,
 }
