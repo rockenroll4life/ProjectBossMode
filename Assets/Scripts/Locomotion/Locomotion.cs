98c7bd86bf5e698297c38646c0e44c067efeae8a
Joshua Letellier <rockenroll4life@gmail.com>
2023-01-29 21:11:27 -0500

    Cleaned up an unneeded variable in the Locomotion

--- Assets/Scripts/Locomotion/Locomotion.cs
+++ Assets/Scripts/Locomotion/Locomotion.cs
@@ -1,22 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class Locomotion : MonoBehaviour {
     //  This values is how fast the player moves per second 6.6 m/s, this value should be tied in with the
     //  animation so we speed up the animation when we're moving faster than normal or slow down when slowed.
-    //  TODO: [Rock]: We need a StateMachineBehavior for our locomotion that adjust the animaiton speed based
+    //  TODO: [Rock]: We need a StateMachineBehavior for our locomotion that adjust the animation speed based
     //  this value and the current movement speed
     //public static readonly float FIXED_MOVEMENT_SPEED = 6.6f;
     public static readonly float FIXED_MOVEMENT_SPEED = 4.95f;
-
     static readonly float ROTATION_SPEED = 720f;
 
     protected NavMeshAgent agent;
     protected Vector3 lookDir;
-    protected Vector3 moveDir;
 
     public void MoveToLocation(Vector3 destination) {
         agent.SetDestination(destination);
@@ -24,7 +20,7 @@ public class Locomotion : MonoBehaviour {
 
     public bool IsMoving() {
         //  We can probably just use Epsilon squared here?
-        return moveDir.sqrMagnitude > Mathf.Epsilon * Mathf.Epsilon;
+        return agent.velocity.sqrMagnitude > (Mathf.Epsilon * Mathf.Epsilon);
     }
 
     public void StopMovement() {
@@ -37,16 +33,12 @@ public class Locomotion : MonoBehaviour {
 
     protected virtual void Update() {
         //  Movement
-        moveDir = agent.velocity;
         if (IsMoving()) {
-            lookDir = moveDir.normalized;
-
-        } else {
-            moveDir = Vector3.zero;
+            lookDir = agent.velocity.normalized;
         }
 
         //  Rotation
-        if (lookDir.sqrMagnitude > Mathf.Epsilon * Mathf.Epsilon) {
+        if (lookDir.sqrMagnitude > (Mathf.Epsilon * Mathf.Epsilon)) {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lookDir), ROTATION_SPEED * Time.deltaTime);
         }
     }
