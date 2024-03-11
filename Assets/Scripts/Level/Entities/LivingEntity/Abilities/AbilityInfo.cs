f9ad9f916e5e8528f718b9ccc32ffe65c78ddf05
rockenroll4life <rockenroll4life@gmail.com>
2024-03-11 12:33:11 -0400

    Messing around with an AbilityInfo namespace

---
+++ Assets/Scripts/Level/Entities/LivingEntity/Abilities/AbilityInfo.cs
@@ -0,0 +1,22 @@
+namespace Ability {
+
+    public class Info {
+        public static int NUM_ABILITIES = 5;
+    }
+
+    public enum Input {
+        NONE = -1,
+        Ability1,
+        Ability2,
+        Ability3,
+        Ability4,
+        Ultimate,
+        _COUNT
+    }
+
+    enum Type {
+
+    }
+
+    
+}
