30fe182f01a33713a643fcab5d84c9d58b7844c9
rockenroll4life <rockenroll4life@gmail.com>
2024-03-16 12:13:00 -0400

    Setup the Keybindings to have support to track controller

--- Assets/Scripts/Settings/KeyBindings.cs
+++ Assets/Scripts/Settings/KeyBindings.cs
@@ -20,65 +20,85 @@ public enum KeyBindingKeys {
     _COUNT,
 }
 
+public enum KeyBindingType {
+    Keyboard,
+    Controller,
+}
+
 public class KeyBindings {
-    private static readonly int SAVE_TABLE_SIZE = 2;
+    static readonly string[] KEYBINDING_LOCATIONS = new string[] {
+            "Settings/Default_Keyboard_KeyBindings.csv",
+            "Settings/Default_Keyboard_ControllerBindings.csv"
+    };
+    static readonly int SAVE_TABLE_SIZE = 2;
 
-    readonly Dictionary<KeyBindingKeys, KeyCode> DEFAULT_BINDINGS = new Dictionary<KeyBindingKeys, KeyCode>();
-    readonly Dictionary<KeyBindingKeys, KeyCode> bindings = new Dictionary<KeyBindingKeys, KeyCode>();
-    
-    public KeyCode GetKeyBinding(KeyBindingKeys keyBindingKey) => bindings[keyBindingKey];
+    readonly Dictionary<KeyBindingType, Dictionary<KeyBindingKeys, KeyCode>> DEFAULT_BINDINGS = new Dictionary<KeyBindingType, Dictionary<KeyBindingKeys, KeyCode>>() {
+        { KeyBindingType.Keyboard, new Dictionary<KeyBindingKeys, KeyCode>() },
+        { KeyBindingType.Controller, new Dictionary<KeyBindingKeys, KeyCode>() }
+    };
 
-    public void ResetKeybindings() {
-        foreach (KeyBindingKeys key in DEFAULT_BINDINGS.Keys) {
-            bindings[key] = DEFAULT_BINDINGS[key];
+    readonly Dictionary<KeyBindingType, Dictionary<KeyBindingKeys, KeyCode>> bindings = new Dictionary<KeyBindingType, Dictionary<KeyBindingKeys, KeyCode>>() {
+        { KeyBindingType.Keyboard, new Dictionary<KeyBindingKeys, KeyCode>() },
+        { KeyBindingType.Controller, new Dictionary<KeyBindingKeys, KeyCode>() }
+    };
+
+    public KeyCode GetKeyBinding(KeyBindingKeys keyBindingKey) => bindings[KeyBindingType.Keyboard][keyBindingKey];
+
+    public void Setup() {
+        foreach (KeyBindingType type in DEFAULT_BINDINGS.Keys) {
+            LoadDefaultCSV(type, KEYBINDING_LOCATIONS[(int) type]);
+
+            //  Check to see if we have an existing key bindings settings file on file..
+            if (SavedSettingsExist(type)) {
+                LoadSavedSettings(type);
+                EventManager.TriggerEvent(GameEvents.Keybindings_Changed);
+            }
+            //  If we don't, we want to load up our keybindings as the default one
+            else {
+                ResetKeybindings(type, false);
+            }
         }
 
         EventManager.TriggerEvent(GameEvents.Keybindings_Changed);
     }
 
-    public void Setup() {
-        LoadDefaultCSV();
-
-        //  Check to see if we have an existing key bindings settings file on file..
-        if (SavedSettingsExist()) {
-            LoadSavedSettings();
-            EventManager.TriggerEvent(GameEvents.Keybindings_Changed);
-        }
-        //  If we don't, we want to load up our keybindings as the default one
-        else {
-            ResetAllKeybindings();
-        }
-
-    }
-
-    bool SavedSettingsExist() {
+    bool SavedSettingsExist(KeyBindingType keybindingType) {
         //  TODO: [Rock]: Actually check if the file exist
         return false;
     }
 
-    void LoadSavedSettings() {
+    void LoadSavedSettings(KeyBindingType keybindingType) {
         //  TODO: [Rock]: Loading up the keybindings saved settings from file
     }
 
-    void LoadDefaultCSV() {
-        TextAsset keyBindingText = StreamingAssetsUtils.LoadTextAsset($"Settings/Default_KeyBindings.csv");
+    void LoadDefaultCSV(KeyBindingType keyBindingType, string settingsLocation) {
+        TextAsset keyBindingText = StreamingAssetsUtils.LoadTextAsset(settingsLocation);
         string[] keyBindings = CSVReaderUtils.ReadCSV(keyBindingText, SAVE_TABLE_SIZE);
+        var defaultData = DEFAULT_BINDINGS[keyBindingType];
 
         int tableSize = keyBindings.Length / SAVE_TABLE_SIZE;
         for (int i = 0; i < tableSize; i++) {
             int offset = (i * SAVE_TABLE_SIZE);
             KeyBindingKeys keyBindingsKey = ParseUtils.Parse<KeyBindingKeys>(keyBindings[offset + 0]);
             KeyCode keyCode = ParseUtils.Parse<KeyCode>(keyBindings[offset + 1]);
-            DEFAULT_BINDINGS.Add(keyBindingsKey, keyCode);
+            defaultData.Add(keyBindingsKey, keyCode);
+        }
+    }
+
+    public void ResetKeybindings(KeyBindingType keyBindingType, bool triggerEvent = true) {
+        var defaultData = DEFAULT_BINDINGS[keyBindingType];
+        foreach (KeyBindingKeys key in defaultData.Keys) {
+            bindings[keyBindingType][key] = defaultData[key];
+        }
+
+        if (triggerEvent) {
+            EventManager.TriggerEvent(GameEvents.Keybindings_Changed);
         }
     }
 
     void ResetAllKeybindings() {
-        bindings.Clear();
-
-        //  If we don't, we want to load up our keybindings as the default one
-        foreach (KeyBindingKeys key in DEFAULT_BINDINGS.Keys) {
-            bindings.Add(key, DEFAULT_BINDINGS[key]);
+        foreach (KeyBindingType type in DEFAULT_BINDINGS.Keys) {
+            ResetKeybindings(type, false);
         }
 
         EventManager.TriggerEvent(GameEvents.Keybindings_Changed);
